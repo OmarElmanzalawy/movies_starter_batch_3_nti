69bc95a5d9c857b324d3d35b32235b47777c667d
Omar Elmanzalawy <omarmanz2002@gmail.com>
2025-10-14 20:41:12 +0300

    send request to api

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:movies_starter/services/api_service.dart';
 import 'package:movies_starter/widgets/movie_card.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    ApiService.fetchPopularMovies();
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,31 @@
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
 class ApiService {
 
+  static final String endpoint = "https://api.themoviedb.org/3/movie/popular";
+  static final String apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNThlZDI2ODU4ZjA4ODYxZDYwZGNkNjdhM2RmYzZmYyIsIm5iZiI6MTc2MDQ0MjA1NC40NzEsInN1YiI6IjY4ZWUzNmM2ODhiYjBlNzg3NTI4MmIyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n8uHiJgPaJ_EuF9rz_CBsktOC3wimTXVZs6ZwF0Oguk";
+
   
+
+  static void fetchPopularMovies()async{
+
+    final Map<String,String> header = {
+    "Authorization": "Bearer $apiKey"
+  };
+
+  final url = Uri.parse(endpoint);
+
+  final response =  await http.get(
+    url,
+    headers: header
+    );
+
+    print(response.body);
+    print(response.statusCode);
+
+  }
+
+
 }
